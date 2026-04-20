80fcbca8698d060a5e55c50cec5e7be67d8dd05e
Carl Avenberg <carl.avenberg@gmail.com>
2026-04-20 11:06:59 +0200

    Plot csv

--- CSV-view/viewer.py
+++ CSV-view/viewer.py
@@ -1,16 +1,29 @@
 import csv
-import numpy
 import os
+import matplotlib.pyplot as plt
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-CSV_FILE = os.path.join(BASE_DIR, 'CSVdata\source-g2theta_target-g4theta.csv')
+CSV_FILE = os.path.join(BASE_DIR, 'CSVdata\source-h2theta_target-h3theta_100steps.csv')
 
-def read_csv(file_path):
+def plot_csv(file_path):
     with open(file_path, 'r') as file:
         reader = csv.reader(file)
-        for row in reader:
-            print(row)  # Or process the row as needed
+        lst = [i for i in reader]
 
-# Example usage
+    phase = []
+    out7 = []
+    out8 = []
+    for row in lst[1:]:
+        phase.append(float(row[0]))
+        out7.append(float(row[2]))
+        out8.append(float(row[4]))
+
+    plt.plot(phase, out7, '-o', label = lst[0][2])
+    plt.plot(phase, out8, '-o', label = lst[0][4])
+    plt.legend()
+    plt.grid()
+    plt.savefig('source-h2theta_target-h3theta_100steps.png')
+    plt.show()
+
 if __name__ == "__main__":
-    read_csv(CSV_FILE)
+    plot_csv(CSV_FILE)
